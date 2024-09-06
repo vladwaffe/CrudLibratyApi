d59a263b8b3bdc93e8da94015c6f6d346952cef2
vlad_waffe <vladwaffe@mail.com>
2024-09-06 09:01:37 +0300

    Initialization of the starting structure of the project

---
+++ libraryservice/src/test/java/com/libraryservice/LibraryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

a0439ab61aa4573833c068b745599f2b80bef12b
vlad_waffe <vladwaffe@mail.com>
2024-09-08 17:40:46 +0300

    Initialization of the starting structure of the project

--- BookTestApi/src/test/java/com/booktestapi/BookTestApiApplicationTests.java
+++ BookTestApi/src/test/java/com/booktestapi/BookTestApiApplicationTests.java
@@ -1,10 +1,10 @@
-package com.libraryservice;
+package com.booktestapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LibraryserviceApplicationTests {
+class BookTestApiApplicationTests {
 
 	@Test
 	void contextLoads() {
--- libraryservice/src/main/java/com/libraryservice/exceptions/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.my.libraryservice.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
